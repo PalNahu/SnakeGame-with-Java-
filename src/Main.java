696107c1f0994950f3dbec9bad7da66ec5dadc75
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2024-02-28 15:39:44 -0300

    Starting Snake Game, the Snake can move

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int ancho = 800;
+        int alto = ancho;
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            JFrame ventana = new JFrame("Snake Game");
+            ventana.setVisible(true);
+            ventana.setSize(800, 800);
+            ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            ventana.setResizable(false);
+            ventana.setLocationRelativeTo(null);
+            ventana.setBackground(Color.black);
+
+            SnakeGame snakeGame = new SnakeGame(ancho, alto);
+            ventana.add(snakeGame);
+            snakeGame.requestFocus();
+        });
+    }
+}
+
+
+
+
+
---
+++ src/SnakeGame.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+    int ancho;
+    int alto;
+
+    public void move() {
+        cabezaSerpiente.x += velocidadX;
+        cabezaSerpiente.y += velocidadY;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            velocidadX = 0;
+            velocidadY = -1;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            velocidadX = 0;
+            velocidadY = 1;
+
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            velocidadX = -1;
+            velocidadY = 0;
+
+        }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            velocidadX = 1;
+            velocidadY = 0;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    int tileSize = 25;
+    Tile cabezaSerpiente;
+
+    Tile comida;
+    Random random;
+
+    Timer loop;
+    int velocidadX;
+    int velocidadY;
+
+    SnakeGame(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+        addKeyListener(this);
+        setFocusable(true);
+        setPreferredSize(new Dimension(this.ancho, this.alto));
+        setBackground(Color.black);
+        cabezaSerpiente = new Tile(5, 5);
+        comida = new Tile(10, 10);
+        random = new Random();
+        colocarComida();
+        loop = new Timer(100, this);
+        loop.start();
+        velocidadX = 0;
+        velocidadY = 0;
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.red);
+        g.fillRect(comida.x * tileSize, comida.y * tileSize, tileSize, tileSize);
+        g.setColor(Color.green);
+        g.fillRect(cabezaSerpiente.x * tileSize, cabezaSerpiente.y * tileSize, tileSize, tileSize);
+
+    }
+
+    public void colocarComida() {
+        comida.x = random.nextInt(ancho / tileSize);
+        comida.y = random.nextInt(alto / tileSize);
+    }
+}
+
+
+
