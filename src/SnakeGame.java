b44822e09646f00de54b2e6a3a8adeb096bdf45c
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2024-03-01 11:23:22 -0300

    The snake have a body and its connected

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SnakeGame extends JPanel implements ActionListener, KeyListener {
@@ -11,7 +12,24 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     int alto;
 
     public void move() {
-        cabezaSerpiente.x += velocidadX;
+        if (colisiónComidaSerpiente(cabezaSerpiente, comida)) {
+            cuerpoSerpiente.add(new Tile(comida.x, comida.y));
+            colocarComida();
+        }
+
+        for (int i = cuerpoSerpiente.size() - 1; i >= 0; i--){
+            Tile parteSerpiente = cuerpoSerpiente.get(i);
+            if(i ==0){
+                parteSerpiente.x = cabezaSerpiente.x;
+                parteSerpiente.y = cabezaSerpiente.y;
+            }else{
+                Tile antParteSerpiente = cuerpoSerpiente.get(i-1);
+                parteSerpiente.x = antParteSerpiente.x;
+                parteSerpiente.y = antParteSerpiente.y;
+            }
+        }
+
+            cabezaSerpiente.x += velocidadX;
         cabezaSerpiente.y += velocidadY;
 
     }
@@ -24,18 +42,18 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        if (e.getKeyCode() == KeyEvent.VK_UP && velocidadY != 1) {
             velocidadX = 0;
             velocidadY = -1;
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN && velocidadY != -1) {
             velocidadX = 0;
             velocidadY = 1;
 
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && velocidadX != 1) {
             velocidadX = -1;
             velocidadY = 0;
 
-        }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && velocidadX != -1) {
             velocidadX = 1;
             velocidadY = 0;
         }
@@ -50,7 +68,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     public void keyReleased(KeyEvent e) {
     }
 
-    private class Tile {
+    private static class Tile {
         int x;
         int y;
 
@@ -62,6 +80,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
 
     int tileSize = 25;
     Tile cabezaSerpiente;
+    ArrayList<Tile> cuerpoSerpiente;
 
     Tile comida;
     Random random;
@@ -78,6 +97,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         setPreferredSize(new Dimension(this.ancho, this.alto));
         setBackground(Color.black);
         cabezaSerpiente = new Tile(5, 5);
+        cuerpoSerpiente = new ArrayList<Tile>();
         comida = new Tile(10, 10);
         random = new Random();
         colocarComida();
@@ -96,14 +116,24 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     public void draw(Graphics g) {
         g.setColor(Color.red);
         g.fillRect(comida.x * tileSize, comida.y * tileSize, tileSize, tileSize);
+
         g.setColor(Color.green);
         g.fillRect(cabezaSerpiente.x * tileSize, cabezaSerpiente.y * tileSize, tileSize, tileSize);
+
+        for (int i = 1; i < cuerpoSerpiente.size(); i++) {
+            Tile parteSerpiente = cuerpoSerpiente.get(i);
+            g.fillRect(parteSerpiente.x * tileSize, parteSerpiente.y * tileSize, tileSize, tileSize);
+        }
 
     }
 
     public void colocarComida() {
         comida.x = random.nextInt(ancho / tileSize);
         comida.y = random.nextInt(alto / tileSize);
+    }
+
+    public boolean colisiónComidaSerpiente(Tile tile1, Tile tile2) {
+        return tile1.x == tile2.x && tile1.y == tile2.y;
     }
 }
 
